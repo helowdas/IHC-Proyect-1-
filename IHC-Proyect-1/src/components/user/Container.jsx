3ecd78a94af69f1d9abc5f7433ea4a7570633cd1
helowdas <pedrodelgadillo199@gmail.com>
2025-10-29 14:45:27 -0400

    added padding-boxshadow-border radius

--- IHC-Proyect-1/src/components/user/Container.jsx
+++ IHC-Proyect-1/src/components/user/Container.jsx
@@ -1,15 +1,21 @@
 // components/user/Container.js
 import React from "react";
-import {FormControl, FormLabel, Slider} from "@mui/material";
+import { FormControl, FormLabel, Slider } from "@mui/material";
 import { Paper } from "@mui/material";
 import { useNode } from "@craftjs/core";
 
-export const Container = ({background, padding = 0, children}) => {
+export const Container = ({ background, padding = 0, borderRadius = 0, boxShadow = "", children }) => {
   const { connectors: { connect, drag } } = useNode();
   return (
-    <Paper 
-      ref={ref => connect(drag(ref))} 
-      style={{margin: "5px 0", background, padding: `${padding}px`}}
+    <Paper
+      ref={ref => connect(drag(ref))}
+      style={{
+        margin: "5px 0",
+        background,
+        padding: `${padding}px`,
+        borderRadius: `${borderRadius}px`,
+        boxShadow: boxShadow ? `0 4px 8px ${boxShadow}` : "none"
+      }}
     >
       {children}
     </Paper>
@@ -17,9 +23,11 @@ export const Container = ({background, padding = 0, children}) => {
 }
 
 export const ContainerSettings = () => {
-  const { background, padding, actions: {setProp} } = useNode(node => ({
+  const { background, padding, borderRadius, boxShadow, actions: { setProp } } = useNode(node => ({
     background: node.data.props.background,
-    padding: node.data.props.padding
+    padding: node.data.props.padding,
+    borderRadius: node.data.props.borderRadius,
+    boxShadow: node.data.props.boxShadow
   }));
   return (
     <div>
@@ -48,13 +56,41 @@ export const ContainerSettings = () => {
           }}
         />
       </FormControl>
+      {/* Nuevo apartado: Decoration */}
+      <FormControl fullWidth={true} margin="normal" component="fieldset">
+        <FormLabel component="legend">Border Radius</FormLabel>
+        <Slider
+          value={typeof borderRadius === 'number' ? borderRadius : 0}
+          step={1}
+          min={0}
+          max={50}
+          valueLabelDisplay="auto"
+          onChange={(_, value) => {
+            const v = Array.isArray(value) ? value[0] : value;
+            if (typeof v === 'number') {
+              setProp(props => props.borderRadius = v)
+            }
+          }}
+        />
+      </FormControl>
+      <FormControl fullWidth={true} margin="normal" component="fieldset">
+        <FormLabel component="legend">Box Shadow</FormLabel>
+        <input
+          type="color"
+          value={boxShadow || '#000000'}
+          onChange={(e) => setProp(props => props.boxShadow = e.target.value)}
+          style={{ width: 40, height: 40, padding: 0, border: 'none', background: 'transparent' }}
+        />
+      </FormControl>
     </div>
   )
 }
 
 export const ContainerDefaultProps = {
-  background : "#ffffff",
-  padding: 3
+  background: "#ffffff",
+  padding: 3,
+  borderRadius: 0,
+  boxShadow: ""
 };
 
 Container.craft = {
